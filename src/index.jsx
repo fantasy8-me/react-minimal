665e1c44ca669f78079877eccd7b131ac59c7c95
eric.miao <fantasy8me@126.com>
2017-09-21 17:55:02 +0800

    minimal react webpack setup

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 // import React from "react";
 import propTypes from 'prop-types';
 
 export default class Index extends React.PureComponent {
   static propTypes = {
-    filed1: propTypes.bool.isRequired,
+    filed1: propTypes.string.isRequired,
   };
   render() {
     const { filed1 } = this.props;
     return <div>{filed1}</div>;
   }
 }
+
+ReactDOM.render(<Index filed1="hello world" />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'stage-2'],
+          },
+        },
+      },
+    ],
+  },
+};
